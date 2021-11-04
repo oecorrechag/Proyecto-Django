0fb70e7da72fd0938cb7d4114bc8924aa2ce042a
Oscar Correcha <oecorrechag@unal.edu.co>
2021-11-04 18:02:25 -0500

    fin video 13

---
+++ posts/models.py
@@ -0,0 +1,29 @@
+""""Users models."""
+
+# Django
+from django.contrib.auth.models import User
+from django.db import models
+
+class Profile(models.Model):
+    """ Profile model.
+        Extension de acción
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    webside = models.URLField(max_length=200, blank=True)
+    bigoraphy = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    picture = models.ImageField(
+        upload_to='user/pictures', 
+        blank=True, 
+        null=True
+        )
+
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Return username"""
+        return self.user.username
+
