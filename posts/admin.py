119ba4e8dba1543a8d037d0313da3c178d5a127f
Oscar Correcha <oecorrechag@unal.edu.co>
2021-11-07 11:03:48 -0500

    fin video 16, reto

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,15 @@
+"""Posts model."""
+
+# Django
 from django.contrib import admin
 
-# Register your models here.
+# Models
+from posts.models import Post
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    """Post admin."""
+
+    list_display = ('id','user','title','photo')
+    search_field = ('title','user__username','user__email')
+    list_filter = ('created','modified')
